239d7c068084f6e1299f888fef58523110c54de4
Qin Li <30631553+liblaf@users.noreply.github.com>
2022-10-30 02:37:06 +0800

    build(scripts): pretty

--- scripts/template.sh
+++ scripts/template.sh
@@ -3,10 +3,14 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-function _exec() {
+function info() {
   echo -e -n "\033[1;94m"
   echo -n "${@}"
   echo -e "\033[0m"
+}
+
+function call() {
+  info "${@}"
   "${@}"
 }
 
@@ -17,12 +21,12 @@ files=(
   "README.md"
 )
 for file in "${files[@]}"; do
-  _exec sed --in-place "s/template/${REPO_NAME}/g" "${file}"
+  call sed --in-place "s/template/${REPO_NAME}/g" "${file}"
 done
 
 files=(.github/workflows/**.yaml)
 for file in "${files[@]}"; do
-  _exec sed --in-place "s/branches-ignore/branches/g" "${file}"
+  call sed --in-place "s/branches-ignore/branches/g" "${file}"
 done
 
-gh repo edit --homepage "https://liblaf.github.io/${REPO_NAME}/"
+call gh repo edit --homepage "https://liblaf.github.io/${REPO_NAME}/"
