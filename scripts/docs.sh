ed311a5d4d2184b6fca0b8eebfdb6585d8f92232
Qin Li <30631553+liblaf@users.noreply.github.com>
2022-10-30 06:39:11 +0800

    build(scripts): fall back to ANSI in absence of rich-cli

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -3,8 +3,20 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+if command -v rich >/dev/null 2>&1; then
+  function info() {
+    rich --print "[bold bright_blue]+ ${*}"
+  }
+else
+  function info() {
+    echo -e -n "\x1b[1;94m"
+    echo -n "+ ${*}"
+    echo -e "\x1b[0m"
+  }
+fi
+
 function call() {
-  rich --print "[bold bright_blue]+ ${*}"
+  info "+ ${*}"
   "${@}"
 }
 
