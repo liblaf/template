c644475d6bfce1d5e701bc1710d8a0e9977bf382
Qin Li <30631553+liblaf@users.noreply.github.com>
2022-12-27 04:03:42 +0800

    feat: initial commit

---
+++ scripts/install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+function exists() {
+  command -v "${@}" > /dev/null 2>&1
+}
+
+function info() {
+  if exists rich; then
+    rich --style "bold bright_blue" --print "${*}"
+  else
+    echo -e -n "\x1b[1;94m"
+    echo -n "${*}"
+    echo -e "\x1b[0m"
+  fi
+}
+
+function success() {
+  if exists rich; then
+    rich --style "bold bright_green" --print "${*}"
+  else
+    echo -e -n "\x1b[1;92m"
+    echo -n "${*}"
+    echo -e "\x1b[0m"
+  fi
+}
+
+function call() {
+  info "+ ${*}"
+  "${@}"
+}
+
+function copy() {
+  mkdir --parents "$(realpath --canonicalize-missing "${2}/..")"
+  cp --force --recursive "${1}" "${2}"
+  success "Copy: ${1} -> ${2}"
+}
+
+if [[ -n ${1-} ]]; then
+  workspace="${1}"
+else
+  workspace="$(git rev-parse --show-toplevel || pwd)"
+fi
+
+call bash "${workspace}/scripts/build.sh"
+mkdir --parents "${HOME}/.local/bin"
+files=("${workspace}/dist"/*)
+for file in "${files[@]}"; do
+  if [[ -x ${file} ]]; then
+    copy "${file}" "${HOME}/.local/bin"
+  fi
+done
