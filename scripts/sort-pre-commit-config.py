d8ae38dbd499251d2685e1526e84ff0fa0fdfcf4
liblaf <i@liblaf.me>
2023-10-26 16:48:18 +0800

    refactor(ci): reorganize pre-commit configuration and add new hooks

--- scripts/sort-pre-commit-config.py
+++ scripts/sort-pre-commit-config.py
@@ -1,10 +1,11 @@
 from pathlib import Path
+from typing import Any
 
 import yaml
 from yaml import CLoader as Loader
 
 config_path: Path = Path(".pre-commit-config.yaml")
-data = yaml.load(stream=config_path.read_text(), Loader=Loader)
+data: dict[str, Any] = yaml.load(stream=config_path.read_text(), Loader=Loader)
 data["repos"].sort(key=lambda x: x["repo"].lower())
 for repo in data["repos"]:
     repo["hooks"].sort(key=lambda x: x["id"].lower())
