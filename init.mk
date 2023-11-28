c110f37fb6e52656e885ef5dd28184691b06ca98
liblaf <i@liblaf.me>
2023-11-28 14:51:45 +0800

    feat: add GitHub workflows for auto-merge, check, and template sync
    This commit adds the following GitHub workflows:
    - `auto-merge.yaml`: This workflow is triggered on pull requests and enables auto-merge for dependabot and pre-commit-ci bots.
    - `check.yaml`: This workflow is triggered on pull requests and checks the status of pending checks. It waits for all pending checks to complete before proceeding. If any checks fail, the workflow fails.
    - `template.yaml`: This workflow is triggered on a schedule and manually. It syncs the current repository with the template repository. It also has the option to push the changes to the current repository.
    
    The purpose of these workflows is to automate the merging process, ensure all checks pass before merging, and keep the current repository in sync with the template repository.

--- init.mk
+++ init.mk
@@ -1,44 +1,36 @@
 SOURCE_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 TARGET_DIR != git rev-parse --show-toplevel
 
-ifeq ($(TARGET_DIR),)
-$(error Fatal: not a git repository (or any of the parent directories): .git)
+ifndef TARGET_DIR
+$(error fatal: not a git repository (or any of the parent directories): .git)
 endif
 
 TARGET_LIST += $(TARGET_DIR)/.github/dependabot.yaml
+TARGET_LIST += $(TARGET_DIR)/.github/workflows/auto-merge.yaml
+TARGET_LIST += $(TARGET_DIR)/.github/workflows/check.yaml
 TARGET_LIST += $(TARGET_DIR)/.github/workflows/template.yaml
 TARGET_LIST += $(TARGET_DIR)/.pre-commit-config.yaml
 
 all: common
-$(info $(TARGET_LIST))
-common: github pre-commit $(TARGET_LIST)
+
+common: $(TARGET_LIST) github pre-commit
 
 ###############
 # Auxiliaries #
 ###############
 
-URL  != git config --get remote.origin.url
-USER != echo $(URL) | sed --regexp-extended --expression='s|.*github.com/(.+)/(.+).git|\1|'
-REPO != echo $(URL) | sed --regexp-extended --expression='s|.*github.com/(.+)/(.+).git|\2|'
+REPO != gh repo view --json=name --template="{{ .name }}"
+USER != gh repo view --json=owner --template="{{ .owner.login }}"
 
 $(TARGET_DIR)/%: $(SOURCE_DIR)/%
 	@- install -D --mode="u=rw,go=r" --no-target-directory --verbose $< $@
 
 github:
-ifeq ($(and $(USER),$(REPO)),)
-	$(warning Unable to determine USER and REPO from git remote origin url: $(URL))
+ifeq ($(and $(USER),$(REPO)), )
+	$(error fatal: not a github repository (or any of the parent directories))
 else
-# https://docs.github.com/en/rest/repos/repos
-	- GH_PAGER="" gh api repos/$(USER)/$(REPO) \
-		--field="allow_merge_commit=false" \
-		--field="delete_branch_on_merge=true" \
-		--method=PATCH
-# https://docs.github.com/en/rest/actions/permissions#set-default-workflow-permissions-for-a-repository
-	- GH_PAGER="" gh api repos/$(USER)/$(REPO)/actions/permissions/workflow \
-		--field="default_workflow_permissions=read" \
-		--field="can_approve_pull_request_reviews=true" \
-		--method=PUT
+	bash $(SOURCE_DIR)/scripts/setup-github.sh $(USER) $(REPO)
 endif
 
 pre-commit: $(TARGET_DIR)/.pre-commit-config.yaml
-	- pre-commit install --install-hooks --hook-type="commit-msg" --hook-type="pre-commit"
+	- pre-commit install --install-hooks --hook-type=commit-msg --hook-type=pre-commit
---
+++ scripts/setup-github.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+user=$1
+repo=$2
+
+# https://docs.github.com/en/rest/repos/repos#update-a-repository
+gh api repos/$user/$repo \
+  --field="allow_merge_commit=false" \
+  --field="allow_auto_merge=true" \
+  --field="delete_branch_on_merge=true" \
+  --method=PATCH
+# https://docs.github.com/en/rest/actions/permissions#set-default-workflow-permissions-for-a-repository
+gh api repos/$user/$repo/actions/permissions/workflow \
+  --field="default_workflow_permissions=read" \
+  --field="can_approve_pull_request_reviews=true" \
+  --method=PUT
+# https://docs.github.com/en/rest/branches/branch-protection#update-branch-protection
+gh api repos/$user/$repo/branches/main/protection \
+  --input=- \
+  --method=PUT <<-EOF
+{
+  "required_status_checks": {
+    "strict": false,
+    "checks": [{ "context": "Check" }]
+  },
+  "enforce_admins": null,
+  "required_pull_request_reviews": null,
+  "restrictions": null,
+  "allow_force_pushes": true
+}
+EOF
