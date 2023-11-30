7d785462fb0408378ad4be2d027ecac6682fef0f
liblaf <i@liblaf.me>
2023-11-30 17:57:48 +0800

    feat: update pre-commit hooks and configurations
    - Removed unused hooks: `cargo-check`, `cargo-clippy`, `cargo-fmt`, `shfmt`
    - Added new hooks: `ruff`, `ruff-format`, `latexindent`
    - Updated versions of existing hooks: `ruff` to v0.1.6, `latexindent` to V3.23.3, `check-jsonschema` to 0.27.2, `poetry` to 1.7.0
    - Updated arguments for hooks: `ruff` with `--fix`, `poetry-export` with `--without-hashes` and `--without-urls`
    - Added new configuration file `.pre-commit-hooks.yaml` with hooks configuration
    - Updated sorting of hooks and repositories in `.pre-commit-config.yaml`
    
    The changes were made to update the pre-commit hooks and configurations in order to improve code quality and formatting.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/liblaf/template
+
+go 1.21
--- scripts/sort-pre-commit-config.py
+++ scripts/sort-pre-commit-config.py
@@ -7,6 +7,4 @@ from yaml import CLoader as Loader
 config_path: Path = Path(".pre-commit-config.yaml")
 data: dict[str, Any] = yaml.load(stream=config_path.read_text(), Loader=Loader)
 data["repos"].sort(key=lambda x: x["repo"].lower())
-for repo in data["repos"]:
-    repo["hooks"].sort(key=lambda x: x["id"].lower())
 config_path.write_text(yaml.dump(data, sort_keys=False))
