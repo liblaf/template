e69151a9e8753f5e3414312ddc8dfc510e2fc2e9
liblaf <30631553+liblaf@users.noreply.github.com>
2024-01-26 14:59:46 +0800

    feat: add GitHub release action
    Add a GitHub release action to create and manage GitHub releases. This action allows users to create a GitHub release, specifying the token, repository, tag, files, release notes, and other release details. The action runs a bash script to handle the release creation process, including generating release notes, marking releases as "Latest" or "Prerelease", and specifying the release title.

---
+++ .github/actions/release/main.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [[ -n $INPUT_TAG ]]; then
+  gh release --repo="$INPUT_REPO" delete "$INPUT_TAG" --cleanup-tag || true
+fi
+
+if [[ -n $INPUT_TAG && -n $INPUT_FILES ]]; then
+  tmpfile=$(mktemp)
+  trap 'rm --verbose $tmpfile' EXIT
+  wget --output-document="$tmpfile" "https://github.com/${INPUT_REPO}/releases/download/${INPUT_TAG}/sha256sums.txt" || true
+  # shellcheck disable=SC2086
+  sha256sum $INPUT_FILES > sha256sums.txt
+  if ! diff "sha256sums.old.txt" "sha256sums.txt" > /dev/null; then
+    exit 0
+  fi
+fi
+
+args=(gh release)
+if [[ -n $INPUT_REPO ]]; then
+  args+=(--repo "$INPUT_REPO")
+fi
+args+=(create)
+if [[ -n $INPUT_TAG ]]; then
+  args+=("$INPUT_TAG")
+fi
+# shellcheck disable=SC2206
+args+=($INPUT_FILES)
+if [[ -f "sha256sums.txt" ]]; then
+  args+=("sha256sums.txt")
+fi
+if [[ "$INPUT_GENERATE_NOTES" ]]; then
+  args+=(--generate-notes)
+fi
+if [[ "$INPUT_LATEST" ]]; then
+  args+=(--latest)
+fi
+if [[ "$INPUT_PRERELEASE" ]]; then
+  args+=(--prerelease)
+fi
+if [[ -n $INPUT_TITLE ]]; then
+  args+=(--title "$INPUT_TITLE")
+elif [[ -n $INPUT_TAG ]]; then
+  args+=(--title "$INPUT_TAG")
+fi
