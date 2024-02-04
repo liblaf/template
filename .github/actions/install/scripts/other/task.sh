92007671721c4e6bcaa8c92cfa41d9a1bc45adf4
liblaf <30631553+liblaf@users.noreply.github.com>
2024-02-05 01:27:25 +0800

    fix(install): ensure ~/.local/bin is added to PATH
    Add a check to verify if ~/.local/bin is already in the PATH environment variable.
    If it is not present, append it to the PATH variable by updating the GITHUB_PATH.
    This ensures that the script can be executed from the ~/.local/bin directory without issues.

--- .github/actions/install/scripts/other/task.sh
+++ .github/actions/install/scripts/other/task.sh
@@ -3,4 +3,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+if [[ ! ":$PATH:" =~ ~/.local/bin: ]]; then
+  echo ~/.local/bin >> "$GITHUB_PATH"
+fi
 sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b ~/.local/bin
